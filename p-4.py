af47e502737b7d8533773403d569ffc8e11933eb
srishti <singhsrishti7562@gmail.com>
2024-09-24 20:31:49 +0530

    Add files via upload

---
+++ p-4.py
@@ -0,0 +1,6 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ADMIN/Desktop/python workshop/p-1.py
+Input the radius of the circle: 1.1
+The area of the circle with radius 1.1 is: 3.8013271108436504
