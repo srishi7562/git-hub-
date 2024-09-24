aaab00086f47d97bf102e8fa5f5258d8a71b49d4
srishti <singhsrishti7562@gmail.com>
2024-09-24 10:39:56 +0530

    Add files via upload

---
+++ p-3.py
@@ -0,0 +1,6 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ADMIN/Desktop/python workshop/p-1.py
+current date and time : 
+24-09-23 20:52:48
