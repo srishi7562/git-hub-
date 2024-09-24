6ef560d0d0ba740db17a087ecd79e09714ce04ad
srishti <singhsrishti7562@gmail.com>
2024-09-24 10:34:55 +0530

    Add files via upload

---
+++ p-2.py
@@ -0,0 +1,8 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ADMIN/Desktop/python workshop/p-1.py
+python version
+3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)]
+version info.
+sys.version_info(major=3, minor=12, micro=6, releaselevel='final', serial=0)
