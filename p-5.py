2a111434974ffe8fd8057c494d427e0be62bf0c5
srishti <singhsrishti7562@gmail.com>
2024-09-26 19:16:34 +0530

    Add files via upload

---
+++ p-5.py
@@ -0,0 +1,7 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ADMIN/Desktop/python workshop/p-1.py
+Input your first name : srishti
+Input your last name : kumari
+Hello kumari srishti
