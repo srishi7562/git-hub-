f7a249716b84bd9636dea34f67959906a13a4fa5
srishti <singhsrishti7562@gmail.com>
2024-09-26 20:16:31 +0530

    Add files via upload

---
+++ input calandar.py
@@ -0,0 +1,8 @@
+# program that prints the calendar for a given month and year.
+
+import calendar
+
+y = int(input("Input the year : "))
+z = int(input("Input the month : "))
+
+print(calendar.month(y, z))
