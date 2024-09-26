076f072917a82dac1a27a13bf5bc6d18d98f7249
srishti <singhsrishti7562@gmail.com>
2024-09-26 19:17:54 +0530

    Add files via upload

---
+++ p-8.py
@@ -0,0 +1,12 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/ADMIN/Desktop/python workshop/p-7.py
+Input the filename: html
+The extension of the file is : 'html'
+>>> 
+============ RESTART: C:/Users/ADMIN/Desktop/python workshop/p-7.py ============
+Traceback (most recent call last):
+  File "C:/Users/ADMIN/Desktop/python workshop/p-7.py", line 2, in <module>
+    print["%s %s" %(colour_list[0], colour_list[-1])]
+TypeError: 'builtin_function_or_method' object is not subscriptable
